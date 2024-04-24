9e46504688147a1361c81f1c828cdbb1a84771ed
RAMZAN KHAN <49532189+theramzankhan@users.noreply.github.com>
2024-04-24 00:48:04 +0000

    Grade Program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,19 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
+// public class Main {
+//   public static void main(String[] args) {
+//     System.out.println("Hello world!!!");
+//   }
+// }
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!!!!!!!!");
+    
+    int grades[]={85,34,97,75,42,86,76,88};
+    
+    for(int i=0;i<grades.length;i++){
+      if(grades[i]<=70){
+        continue;
+      }
+      System.out.println("Passing grades of course number : "+ i + " " + grades[i]);
+    }
   }
-
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
 }
